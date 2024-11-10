be110966cfac852917267cb25e3ff9e75f2e3cbf
andgineer <Andrei_Sorokin2@epam.com>
2024-11-10 07:05:16 +0100

    verup for actions

--- scripts/verup.sh
+++ scripts/verup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Increments version git tag or create snapshot version from the release current commit was branched from.
+# Increments version git tag and set it in reduced form (one digit for release, two for feature etc).
 # Saves the version into files listed in VERSION_FILES (space-separated)
+#
 # Usage:
-#   verup.sh release / feature / branch / ss
-# (ss - to create snapshot version)
+#   verup.sh release / feature / branch
 
 VERSION_FILES=("version")
 
@@ -17,30 +17,23 @@ if [[ $(git diff-index HEAD) || $(git status) == *"is ahead"* ]]; then
   exit -1
 fi
 
-if [[ "$1" == "ss" ]]; then
-  # get the latest non-snapshot backward from current commit
-  TAG=$(git describe --match "v[0-9]*" --abbrev=0 || echo "")
-else
-  # force fetching tags from all branches
-  git fetch --tags
-  # get the latest by creation, from all branches. note! "[0-9]*" does not repeat square brackets -
-  # this is "one digit and any number of any chars".
-  TAG=$(git describe \
-    --match "v[0-9]*.[0-9]*.[0-9]*" \
-    --abbrev=0 \
-    --tags "$(git rev-list --tags --max-count=1)" \
-    || echo "")
-fi
+# force fetching tags from all branches
+git fetch --tags
+TAG=$(git describe \
+  --match "v[0-9]*" \
+  --abbrev=0 \
+  --tags "$(git rev-list --tags --max-count=1)" \
+  || echo "")
 
 major=0
 minor=0
 build=0
 
-regex="v([0-9]+)\.([0-9]+)\.([0-9]+)"
+regex="v([0-9]+)(\.([0-9]+))?(\.([0-9]+))?"
 if [[ $TAG =~ $regex ]]; then
-  major="${BASH_REMATCH[1]}"
-  minor="${BASH_REMATCH[2]}"
-  build="${BASH_REMATCH[3]}"
+    major="${BASH_REMATCH[1]}"
+    minor="${BASH_REMATCH[3]:-0}"
+    build="${BASH_REMATCH[5]:-0}"
 fi
 
 echo -e "Last version: \033[33m$major.$minor.$build\033[39m"
@@ -49,20 +42,19 @@ if [[ "$1" == "release" ]]; then
   build=0
   minor=0
   major=$(echo $major + 1 | bc)
+  NEW_VERSION=$(echo "$major")
 elif [[ "$1" == "feature" ]]; then
   build=0
   minor=$(echo $minor + 1 | bc)
+  NEW_VERSION=$(echo "$major.$minor")
 elif [[ "$1" == "bug" ]]; then
   build=$(echo $build + 1 | bc)
-elif [[ "$1" == "ss" ]]; then
-  major="i$major"
-  build="$build-$(git rev-parse --short HEAD)"
+  NEW_VERSION=$(echo "$major.$minor.$build")
 else
-  echo "usage: ./verup.sh [release|feature|bug|ss]"
+  echo "usage: ./verup_action.sh [release|feature|bug]"
   exit -1
 fi
 
-NEW_VERSION=$(echo "$major.$minor.$build")
 NEW_TAG=$(echo "v$NEW_VERSION")
 echo -e "New version tag: \033[32m$NEW_TAG\033[39m"
 
@@ -91,3 +83,7 @@ echo "...push"
 git tag $NEW_TAG -m "$COMMIT_MSG"
 git push origin $NEW_TAG
 git push
+
+# Updating local tags
+git tag -d $NEW_TAG
+git fetch --prune --tags
